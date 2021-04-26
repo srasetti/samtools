f70feecc034b29545aa615be22f63b06c5c126d7
Samuel Rasetti <srasetti@yahoo.com>
2021-04-26 17:27:44 -0400

    updated app file

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import jsonify
 import sqlite3 as sql
 
 # Create the application.
-APP = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
-@APP.route('/')
+@app.route('/')
 def index():
     """ Displays the index page accessible at '/'
     """
     return flask.render_template('index.html')
 
 if __name__ == '__main__':
-    APP.debug=True
-    APP.run()
+    app.debug=True
+    app.run()
